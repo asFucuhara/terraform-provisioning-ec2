9d6e5565cb7e0b2d34c5b59bfff411f322cb9c54
André Shindi Fucuhara <asfucuhara@live.com>
2024-08-20 15:42:26 -0300

    feat: initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {}
+
+variable cidr_blocks {}
+
+variable avail_zone {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.cidr_blocks[0].cidr_block
+  tags = {
+    Name: var.cidr_blocks[0].name
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.cidr_blocks[1].cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: var.cidr_blocks[1].name
+  }
+}
