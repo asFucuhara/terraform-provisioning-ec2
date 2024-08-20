3f845c9c6659a352c3152020a5b18b37f41c28e4
André Shindi Fucuhara <asfucuhara@live.com>
2024-08-20 16:12:34 -0300

    feat: basic network config

--- main.tf
+++ main.tf
@@ -1,21 +1,65 @@
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
-variable cidr_blocks {}
-
+variable vpc_cidr_blocks {}
+variable subnet_cidr_blocks {}
 variable avail_zone {}
+variable env_prefix {}
 
 resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+  cidr_block = var.vpc_cidr_blocks
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
+  cidr_block = var.subnet_cidr_blocks
   availability_zone = var.avail_zone
   tags = {
-    Name: var.cidr_blocks[1].name
+    Name: "${var.env_prefix}-vpc-subnet-1"
+  }
+}
+
+# create a new route table 
+# resource "aws_route_table" "myapp-route-table"  {
+#   vpc_id = aws_vpc.myapp-vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-ig.id
+#   }
+
+#   tags = {
+#     Name: "${var.env_prefix}-rtb"
+#   }
+# }
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# use auto generated table
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-ig.id
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+} 
+
+resource "aws_internet_gateway" "myapp-ig" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name: "${var.env_prefix}-igw"
   }
 }
