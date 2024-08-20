61a6c355d3a68a07f85537f6f4af82a59d44e1db
André Shindi Fucuhara <asfucuhara@live.com>
2024-08-20 16:19:39 -0300

    feat: adding security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -61,5 +62,36 @@ resource "aws_internet_gateway" "myapp-ig" {
 
   tags = {
     Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name= "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
   }
 }
